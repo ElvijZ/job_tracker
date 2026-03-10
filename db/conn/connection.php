090f7c43a8b59d2ec9fa8a9a5b44127433d168e9
ElvijZ <zepselvijs18@gmail.com>
2026-03-10 22:40:09 +0200

    master added connection to database

---
+++ db/conn/connection.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "db";
+$username = "root";
+$password = "root";
+$dbname = "jobtracker";
+
+// Create connection
+try{
+    $conn =  new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();  
+}
+
